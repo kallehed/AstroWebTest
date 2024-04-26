1062598906d2c63ff6aec9b6151935b2365e7e39
kallehed <kallehed@gmail.com>
2024-04-26 07:24:10 +0200

    change

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'preact/hooks';
+
+export default function Greeting({messages}) {
+
+    const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
+
+    const [greeting, setGreeting] = useState(messages[0]);
+
+    return (
+        <div>
+            <h3>{greeting}! Thank you for le visit!</h3>
+            <button onClick={() => setGreeting(randomMessage())} style="
+		font-weight: 400;
+		text-align: center;
+		white-space: nowrap;
+		vertical-align: middle;
+		user-select: none;
+		border: 1px solid transparent;
+		padding: .375rem .75rem;
+		font-size: 1rem;
+		line-height: 1.5;
+		border-radius: .25rem;
+		
+	">
+                New greeting
+            </button>
+        </div>
+    );
+}
